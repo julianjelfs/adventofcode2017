b053404bf8a7aaa11d399e30f45d862c950a230d
jjelfs <jjelfs@travelrepublic.co.uk>
2017-12-02 08:35:40 +0000

    day2 part one

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,4 +1,24 @@
 module Day2 where
 
+import Data.Char
+
 main :: IO ()
 main = putStrLn("hello")
+
+numbers :: IO [[Int]]
+numbers = do
+  inp <- readFile "data/day2.txt"
+  return $ sanitise $ lines inp
+
+diff :: [Int] -> Int
+diff row =
+  (maximum row) - (minimum row)
+
+sanitise :: [String] -> [[Int]]
+sanitise rows =
+  (fmap . fmap) read $ fmap words rows
+
+solve :: IO Int
+solve = do
+  n <- numbers
+  return $ sum $ fmap diff n
