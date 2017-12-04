17246240aeae4374b541d9c8ab1222d613831d64
MERIDIAN\jjelfs <jjelfs@travelrepublic.co.uk>
2017-12-04 14:20:46 +0000

    much nicer

--- src/Day4.hs
+++ src/Day4.hs
@@ -2,46 +2,25 @@ module Day4 where
 
 import qualified Data.Set as S
 import Data.List
-import Debug.Trace
 
-type Policy = [String] -> Bool
-
-phrases :: IO [[String]]
-phrases = do
+phrases :: (String -> [[String]]) -> IO [[String]]
+phrases parser = do
   inp <- readFile "data/day4.txt"
-  return $ parse inp
+  return $ parser inp
 
-parse :: String -> [[String]]
-parse = (fmap words) . lines
-
-isAnagram :: String -> String -> Bool
-isAnagram a b = sort a == sort b
-
-noDuplicates :: Policy
+noDuplicates :: [String] -> Bool
 noDuplicates p =
   length p == (length (S.fromList p))
 
-noAnagrams :: Policy
-noAnagrams p =
-  length (anagrams p) == 0
-
-both :: Policy
-both p =
-    (noDuplicates p) && (noAnagrams p)
-
-anagrams :: [String] -> [(String, String)]
-anagrams p =
-    [(x, y) | x <- p, y <- p, x /= y, y /= x, isAnagram x y]
-
-countValid :: Policy -> IO Int
-countValid policy = do
-  p <- phrases
-  return $ length $ filter policy p
+countValid :: (String -> [[String]]) -> IO Int
+countValid parser = do
+  p <- phrases parser
+  return $ length $ filter noDuplicates p
 
 partOne :: IO Int
 partOne =
-  countValid noDuplicates
+  countValid $ (fmap words) . lines
 
 partTwo :: IO Int
 partTwo =
-  countValid both
+  countValid $ ((fmap . fmap) sort) . (fmap words) . lines
