db66f1220806ecd24326828f8b7459edf08029ad
MERIDIAN\jjelfs <jjelfs@travelrepublic.co.uk>
2017-12-06 08:32:35 +0000

    switched let to where - don't know which is more idiomatic

--- src/Day6.hs
+++ src/Day6.hs
@@ -10,23 +10,21 @@ testInput = V.fromList [0,2,7,0]
 maxi xs = V.maximumBy (comparing fst) (V.zip xs (V.fromList [0..(length xs)]))
 
 balance v =
-  let
+  distribute m (next i v1) v1
+  where
     (m, i) = maxi v
     v1 = v V.// [(i, 0)]
-  in distribute m (next i v1) v1
 
 distribute 0 i v = v
 distribute m i v =
   distribute (m - 1) (next i v) (v V.// [(i, (v V.! i) + 1)])
 
 findCycle previous v cycles =
-  let
-    v1 = balance v
-  in
-    if M.member v1 previous then
-      (cycles, cycles - (previous M.! v1))
-    else
-      findCycle (M.insert v1 cycles previous) v1 (cycles + 1)
+  if M.member v1 previous then
+    (cycles, cycles - (previous M.! v1))
+  else
+    findCycle (M.insert v1 cycles previous) v1 (cycles + 1)
+  where v1 = balance v
 
 solve =
   findCycle M.empty input 1
