0f8d01ef2573a64c87202875f4b7b9a597842526
jjelfs <jjelfs@travelrepublic.co.uk>
2017-12-07 08:38:41 +0000

    day 7 part one

--- src/Day7.hs
+++ src/Day7.hs
@@ -1,19 +1,62 @@
 module Day7 where
 
+import Data.List.Split
+
 type Name = String
 type Weight = Int
 type Children = [Node]
 
 data Node = Node Name Weight Children deriving Show
 
+removeCommas :: [String] -> [String]
+removeCommas ws =
+  fmap (filter (\c -> c /= ',')) ws
+
 parse :: IO [Node]
 parse = do
   inp <- readFile "data/day7.txt"
-  return $ fmap (lineToNode . words) $ lines inp
+  --inp <- readFile "data/day7test.txt"
+  return $ fmap (lineToNode . removeCommas . words) $ lines inp
 
 lineToNode :: [String] -> Node
 lineToNode (name : weight : _ : children) =
   Node name (read weight :: Int) (fmap (\c -> Node c 0 []) children)
 lineToNode (name : weight) =
   Node name (read (head weight) :: Int) []
+
+--------------------------------------------
+-- find the node that doesn't have a parent
+-- for each node check whether it is a child of any other node
+-- if it is not, it is our root
+--------------------------------------------
+
+isChildOf :: Node -> Node -> Bool
+isChildOf (Node name _ _) (Node _ _ children) =
+  elem name (fmap (\(Node name _ _) -> name) children)
+
+findParents :: [Node] -> [(Node, Maybe Node)]
+findParents nodes =
+  fmap (findParent nodes) nodes
+
+findParent :: [Node] -> Node -> (Node, Maybe Node)
+findParent nodes child =
+  case filter (\n -> isChildOf child n) nodes of
+    (x : xs) -> (child, Just x)
+    [] -> (child, Nothing)
+
+isRoot :: [Node] -> Node -> Bool
+isRoot nodes node =
+  (length $ filter (\n -> isChildOf node n) nodes) == 0
+
+
+solve = do
+  nodes <- parse
+  let h = head nodes
+  --return $ fmap (\n -> (n, isChildOf h n)) nodes
+  return $ filter (\(n, r) -> r) $ fmap (\n -> (n, isRoot nodes n)) nodes
+
+
+
+
+
 
