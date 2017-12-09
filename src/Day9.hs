40d1e9ed5892bc76f57705352f9dfa8522fa9d75
jjelfs <jjelfs@travelrepublic.co.uk>
2017-12-09 10:07:01 +0000

    is this better?

--- src/Day9.hs
+++ src/Day9.hs
@@ -4,19 +4,11 @@ solve = do
   inp <- readFile "data/day9.txt"
   return $ foldl foldData (0, 0, False, False, 0) inp
 
-foldData acc@(level, score, ingarbage, skip, total) ch =
-  case ingarbage of
-    False ->
-      case ch of
-        '{' -> (level + 1, score + level + 1, ingarbage, False, total)
-        '}' -> (level - 1, score, False, False, total)
-        '<' -> (level, score, True, False, total)
-        _ -> acc
-    True ->
-      case skip of
-        True -> (level, score, True, False, total)
-        False ->
-          case ch of
-            '!' -> (level, score, True, True, total)
-            '>' -> (level, score, False, False, total)
-            _ -> (level, score, ingarbage, skip, total + 1)
+foldData (level, score, False, _, total) '{' = (level + 1, score + level + 1, False, False, total)
+foldData (level, score, False, _, total) '}' = (level - 1, score, False, False, total)
+foldData (level, score, False, _, total) '<' = (level, score, True, False, total)
+foldData acc@(_, _, False, _, _) _ = acc
+foldData (level, score, True, True, total) _ = (level, score, True, False, total)
+foldData (level, score, True, False, total) '!' = (level, score, True, True, total)
+foldData (level, score, True, False, total) '>' = (level, score, False, False, total)
+foldData (level, score, True, False, total) _ = (level, score, True, False, total + 1)
