f7dc2b525cc7ed7dabdf9e6fd1b43aa814d737c6
jjelfs <jjelfs@travelrepublic.co.uk>
2017-12-10 15:10:46 +0000

    tidy up

--- src/Day10.hs
+++ src/Day10.hs
@@ -3,20 +3,17 @@ module Day10 where
 import           Data.Bits       (xor)
 import           Data.Char       (ord)
 import           Data.List.Split
-import qualified Data.Vector     as V
+import           Data.Vector     (fromList, toList, (!), (//))
 import           Numeric         (showHex)
 
 lengths = "94,84,0,79,2,27,81,1,123,93,218,23,103,255,254,243"
 
-suffix = [17, 31, 73, 47, 23]
+input = fromList [0 .. 255]
 
 parse str = fmap (\n -> read n :: Int) $ splitOn "," str
 
-parseAscii str = foldl (\xs n -> xs ++ l) [] [0 .. 63]
-  where
-    l = fmap ord str ++ suffix
-
-input = V.fromList [0 .. 255]
+parseAscii str =
+  concat $ take 64 $ repeat $ fmap ord str ++ [17, 31, 73, 47, 23]
 
 partOne = foldl hash (input, 0, 0) $ parse lengths
 
@@ -26,20 +23,18 @@ padleft str
 
 partTwo =
   let (v, _, _) = foldl hash (input, 0, 0) $ parseAscii lengths
-      chunked = chunksOf 16 (V.toList v)
+      chunked = chunksOf 16 (toList v)
       hex =
         fmap ((\n -> padleft $ showHex n "") . foldl xor (0 :: Integer)) chunked
   in foldl (++) mempty hex
 
 hash (v, pos, skip) l =
-  let updated = replaceSlice v pos l
-      nextPos = wrapIndex 256 $ pos + l + skip
-  in (updated, nextPos, skip + 1)
+  (replaceSlice v pos l, wrapIndex 256 $ pos + l + skip, skip + 1)
 
 wrapIndex l n = mod n l
 
 replaceSlice v from n =
   let indices = fmap (wrapIndex $ length v) [from .. (from + n - 1)]
-      slice = fmap (v V.!) indices
+      slice = fmap (v !) indices
       reversed = zip indices (reverse slice)
-  in foldl (\v p -> v V.// [p]) v reversed
+  in foldl (\v p -> v // [p]) v reversed
