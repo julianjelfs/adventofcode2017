187463743ccb9294bd3980c15a65440c323c21a8
MERIDIAN\jjelfs <jjelfs@travelrepublic.co.uk>
2017-12-12 08:51:03 +0000

    day12

--- src/Day12.hs
+++ src/Day12.hs
@@ -4,20 +4,34 @@ import           Data.List.Split
 import qualified Data.Set as S
 import qualified Data.Map.Strict as M
 
-solve = do
-  inp <- readFile "data/day12test.txt"
-  let m = foldl (\m l -> parse m $ words l) M.empty (lines inp)
-      v = partOne m S.empty "0"
-  return $ length v
+partOne = do
+  m <- parse
+  return $ length $ findGroup m S.empty "0"
 
-partOne m visited k =
+partTwo = do
+  m <- parse
+  return $ countGroups m (S.fromList $ M.keys m) 0
+
+countGroups m remaining groups
+    | length remaining == 0 = groups
+    | otherwise =
+        let v = findGroup m S.empty (head $ S.toList remaining)
+            d = S.difference remaining v
+        in
+            countGroups m d (groups + 1)
+
+findGroup m visited k =
   let connected = m M.! k
       v = S.insert k visited
       unvisited = S.difference connected v
   in
     case length unvisited of
       0 -> v
-      _ -> foldl (\v u -> partOne m v u) v unvisited
+      _ -> foldl (\v u -> findGroup m v u) v unvisited
 
-parse m (p:"<->":ps:[]) = M.insert p (S.fromList $ splitOn ", " ps) m
-parse _ _ = error "unexpected format"
+parse = do
+  inp <- readFile "data/day12.txt"
+  return $ foldl (\m l -> parseLine m $ words l) M.empty (lines inp)
+
+parseLine m (p:"<->":ps:[]) = M.insert p (S.fromList $ splitOn "," ps) m
+parseLine _ _ = error "unexpected format"
