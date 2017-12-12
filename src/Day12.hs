989261e49a324403a9720909382ab49d157d3cdd
jjelfs <jjelfs@travelrepublic.co.uk>
2017-12-12 07:47:29 +0000

    making a meal of this

--- src/Day12.hs
+++ src/Day12.hs
@@ -7,17 +7,17 @@ import qualified Data.Map.Strict as M
 solve = do
   inp <- readFile "data/day12test.txt"
   let m = foldl (\m l -> parse m $ words l) M.empty (lines inp)
-  return $ partOne m S.empty "0"
+      v = partOne m S.empty "0"
+  return $ length v
 
-partOne :: M.Map String (S.Set String) -> S.Set String -> String -> Int
 partOne m visited k =
   let connected = m M.! k
       v = S.insert k visited
       unvisited = S.difference connected v
   in
     case length unvisited of
-      0 -> 0
-      _ -> sum $ fmap (partOne m v) unvisited
+      0 -> v
+      _ -> foldl (\v u -> partOne m v u) v unvisited
 
 parse m (p:"<->":ps:[]) = M.insert p (S.fromList $ splitOn ", " ps) m
 parse _ _ = error "unexpected format"
