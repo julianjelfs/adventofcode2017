8c7951fa1018527b5d20d16f727a292d919cc6a5
jjelfs <jjelfs@travelrepublic.co.uk>
2017-12-13 20:06:21 +0000

    tweaks

--- src/Day13.hs
+++ src/Day13.hs
@@ -1,36 +1,34 @@
 module Day13 where
 
-import           Data.List.Split
-import           Data.List
+import Data.List
+import Data.List.Split
 import qualified Data.Map.Strict as M
 
-partOne = do
-  inp <- readFile "data/day13.txt"
-  let scanners = parse inp
-  return $ trip scanners 0
+partOne = (\s -> trip s 0) <$> parse <$> readFile "data/day13.txt"
 
 partTwo = do
   inp <- readFile "data/day13.txt"
   let scanners = parse inp
-      trips = fmap (trip scanners) [0..]
+      trips = fmap (trip scanners) [0 ..]
   return $ take 1 $ filter (\(n, (c, s)) -> not c) $ trips
 
 parse inp =
-    foldr foldLine M.empty ((fmap (read::String->Int) . splitOn ": ") <$> lines inp)
-    where
-        foldLine (d:r:[]) m = M.insert d r m
-        foldLine _ _ = error "invalid format"
+  foldr
+    foldLine
+    M.empty
+    ((fmap (read :: String -> Int) . splitOn ": ") <$> lines inp)
+  where
+    foldLine (d:r:[]) m = M.insert d r m
+    foldLine _ _ = error "invalid format"
 
-trip scanners delay =
-    (delay, foldl' (step delay scanners) (False, 0) [0..90])
+trip scanners delay = (delay, foldl' (step delay scanners) (False, 0) [0 .. 90])
 
-isCaught delay range =
-    rem delay (2 * (range - 1)) == 0
+isCaught delay range = rem delay (2 * (range - 1)) == 0
 
 step delay scanners (caught, sev) depth =
-    case M.lookup depth scanners of
-        Nothing -> (caught || False, sev)
-        Just r ->
-            case isCaught (delay + depth) r of
-                True -> (True, sev + (depth * r))
-                _ -> (caught || False, sev)
+  case M.lookup depth scanners of
+    Nothing -> (caught || False, sev)
+    Just r ->
+      case isCaught (delay + depth) r of
+        True -> (True, sev + (depth * r))
+        _ -> (caught || False, sev)
