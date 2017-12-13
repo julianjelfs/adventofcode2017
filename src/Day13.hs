714ab72eee3557216d40fa7c2527d9fbbe0b5716
MERIDIAN\jjelfs <jjelfs@travelrepublic.co.uk>
2017-12-13 12:33:07 +0000

    day13 can fuck right off

--- src/Day13.hs
+++ src/Day13.hs
@@ -1,39 +1,50 @@
 module Day13 where
 
 import           Data.List.Split
+import           Data.List
+import qualified Data.Map.Strict as M
 
-partOne = do
+partTwo = do
   inp <- readFile "data/day13.txt"
   let scanners = (parseScanner . fmap (read::String->Int) . splitOn ": ") <$> lines inp
-  return $ foldl step (0, 0, scanners) [0..90]
+      mapped = foldr parseScanner2 M.empty scanners
+      delayed = (delayBy mapped) <$> [0..]
+  return $ take 1 $ filter (\(n, c) -> not c) $ trip <$> delayed
 
-partTwo n = do
-  inp <- readFile "data/day13test.txt"
-  --inp <- readFile "data/day13.txt"
-  let scanners = (parseScanner . fmap (read::String->Int) . splitOn ": ") <$> lines inp
-      delayed = (delayBy scanners) <$> [0..]
-      (_, byTen) = delayBy scanners n
-
-  --return $ head $ filter (\(n, (_, s, _)) -> s == 0) $ fmap (\(n, s) -> (n, foldl step (0, 0, s) [0..6])) delayed
-  return $ foldl step (0, 0, byTen) [0..6]
+parseScanner2 (d, r, p, dir) m = M.insert d (r, p, dir) m
 
 parseScanner (d:r:[]) = (d, r, 0, 1)
 parseScanner _ = error "invalid format"
 
 delayBy scanners n =
-  (n, foldl (\s i -> moveScanner <$> s) scanners [1..n])
+  (n, scanners)
 
-step (myDepth, severity, scanners) pico =
-  ( myDepth + 1
-  , severity + (accumulateSeverity myDepth scanners)
-  , moveScanner <$> scanners )
+trip (delay, scanners) =
+    (delay, foldl' (step delay scanners) False [0..90])
 
-accumulateSeverity myDepth scanners =
-  case filter (\(d, r, p, _) -> d == myDepth && p == 0) scanners of
-    ((d, r, _, _):[]) -> d * r
-    _ -> 0
+isCaught delay (range, _, _) =
+    rem delay (2 * (range - 1)) == 0
 
-moveScanner (depth, range, pos, dir)
-  | pos == range - 1 && dir == 1 = (depth, range, pos - 1, -1)
-  | pos == 0 && dir == -1 = (depth, range, pos + 1, 1)
-  | otherwise = (depth, range, pos + dir, dir)
+step delay scanners caught depth =
+    if caught then
+        caught
+    else
+        case isCaught (delay + depth) <$> M.lookup depth scanners of
+            Just True -> True
+            _ -> caught || False
+
+--partOne = do
+--  inp <- readFile "data/day13.txt"
+--  let scanners = (parseScanner . fmap (read::String->Int) . splitOn ": ") <$> lines inp
+--      mapped = foldr parseScanner2 M.empty scanners
+--  return $ trip (0, mapped)
+
+--accumulateSeverity myDepth scanners =
+--  case M.lookup myDepth scanners of
+--    Just (r, 0, _) -> ((myDepth * r), True)
+--    _ -> (0, False)
+
+--moveScanner (range, pos, dir)
+--  | pos == range - 1 && dir == 1 = (range, pos - 1, -1)
+--  | pos == 0 && dir == -1 = (range, pos + 1, 1)
+--  | otherwise = (range, pos + dir, dir)
--- src/Main.hs
+++ src/Main.hs
@@ -3,8 +3,9 @@ module Main where
 import           Day4
 import           Day5
 import           Day7
+import           Day13
 
 main :: IO ()
 main = do
-  n <- Day7.partTwo
+  n <- Day13.partTwo
   putStrLn ("Solution " ++ (show n))
