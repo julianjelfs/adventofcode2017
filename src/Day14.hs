6b81cccdd4e129860bf2f45cac5497530c76c216
MERIDIAN\jjelfs <jjelfs@travelrepublic.co.uk>
2017-12-14 11:04:10 +0000

    progress

--- src/Day14.hs
+++ src/Day14.hs
@@ -5,9 +5,12 @@ import Numeric
 import Day10
 import qualified Data.Set as S
 import Data.List
+import Data.Maybe
 
 inp = "hwlqcszp-"
 hashes = ((inp ++) . show) <$> [0..127]
+
+type Coords = S.Set (Int, Int)
 
 partOne =
   length $ filter (\c -> c == '1') $ (concatMap . concatMap) hexToBinary $ Day10.doHash <$>hashes
@@ -19,7 +22,21 @@ partTwo = do
                         (\(s, y) l ->
                             (fst $ foldl' (\(s, x) c -> (if c == '1' then S.insert (x, y) s else s, x + 1)) (s, 0) l, y + 1)
                         ) (S.empty, 0) $ lines grid
-    return coords
+    return $ coords
+
+--answer is too high 1302
+countGroups :: Int -> Coords -> Int
+countGroups n coords
+    | length coords == 0 = n
+    | otherwise =
+        let h = (head . S.toList) coords
+        in countGroups (n + 1) $ S.difference coords $ S.fromList $ neighbours coords h
+
+neighbours :: Coords -> (Int, Int) -> [(Int, Int)]
+neighbours coords (x, y) =
+    (x, y) : (concatMap (neighbours updatedCoords) $ around)
+    where around = filter (\c -> S.member c coords) [(x+1, y), (x, y-1), (x, y+1)]
+          updatedCoords = S.difference coords $ S.fromList around
 
 hexToBinary c =
   case (readHex::String -> [(Int, String)]) [c] of
