2cd964c99c916d28597fe62c52ebd72e1c955adb
jjelfs <jjelfs@travelrepublic.co.uk>
2017-12-18 19:51:10 +0000

    tidy up - still horrible

--- src/Day18.hs
+++ src/Day18.hs
@@ -1,140 +1,143 @@
 module Day18 where
 
 import qualified Common as C
-import qualified Data.Map.Strict as M
-import qualified Text.Parsec as P
 import Data.Char
+import qualified Data.Map.Strict as M
 import qualified Data.Vector as V
-import Control.Concurrent (forkIO, threadDelay)
-import Control.Concurrent.MVar (newEmptyMVar, takeMVar, putMVar)
-import Control.Concurrent.Chan (newChan, writeChan, readChan)
-import Data.Foldable (for_)
-import Control.Monad (forever)
-import Control.Exception
+import qualified Text.Parsec as P
 
 data InstrType
   = Number Int
   | Register Char
-  deriving Show
+  deriving (Show)
 
 data Instr
   = Snd InstrType
-  | Set Char InstrType
-  | Add Char InstrType
-  | Mul Char InstrType
-  | Mod Char InstrType
+  | Set Char
+        InstrType
+  | Add Char
+        InstrType
+  | Mul Char
+        InstrType
+  | Mod Char
+        InstrType
   | Rcv Char
-  | Jgz InstrType InstrType
-  deriving Show
+  | Jgz InstrType
+        InstrType
+  deriving (Show)
 
+data Program = Program
+  { progId :: Int
+  , register :: M.Map Char Int
+  , pos :: Int
+  , sent :: Int
+  , waiting :: Bool
+  , finished :: Bool
+  , queue :: [Int]
+  , lastSent :: Int
+  }
 
 parse = do
   inp <- readFile "data/day18.txt"
   return $ fmap V.fromList $ C.parse (P.sepBy instrParser (P.char '\n')) inp
 
-targetParser =
-  P.choice [(Number <$> C.numberParser), (Register <$> P.anyChar)]
+targetParser = P.choice [(Number <$> C.numberParser), (Register <$> P.anyChar)]
 
 instrParser =
   P.choice
     [ P.try $ P.string "snd " *> (Snd <$> targetParser)
-    , P.try $ P.string "set " *> (Set <$> (P.anyChar <* P.char ' ') <*> targetParser)
-    , P.try $ P.string "add " *> (Add <$> (P.anyChar <* P.char ' ') <*> targetParser)
-    , P.try $ P.string "mul " *> (Mul <$> (P.anyChar <* P.char ' ') <*> targetParser)
-    , P.try $ P.string "mod " *> (Mod <$> (P.anyChar <* P.char ' ') <*> targetParser)
+    , P.try $
+      P.string "set " *> (Set <$> (P.anyChar <* P.char ' ') <*> targetParser)
+    , P.try $
+      P.string "add " *> (Add <$> (P.anyChar <* P.char ' ') <*> targetParser)
+    , P.try $
+      P.string "mul " *> (Mul <$> (P.anyChar <* P.char ' ') <*> targetParser)
+    , P.try $
+      P.string "mod " *> (Mod <$> (P.anyChar <* P.char ' ') <*> targetParser)
     , P.try $ P.string "rcv " *> (Rcv <$> P.anyChar)
-    , P.try $ P.string "jgz " *> (Jgz <$> (targetParser <* P.char ' ') <*> targetParser)
+    , P.try $
+      P.string "jgz " *> (Jgz <$> (targetParser <* P.char ' ') <*> targetParser)
     ]
-
-processInstruction progId instr reg freq i
-    | i < 0 || i >= (length instr) = freq
-    | otherwise =
-        case instr V.! i of
-            Rcv n -> if (getValue progId reg (Register n)) > 0 then freq else (processInstruction progId instr reg freq (i + 1))
-            Snd n -> (processInstruction progId instr reg (getValue progId reg n) (i + 1))
-            Set a b -> (processInstruction progId instr (M.insert a (getValue progId reg b) reg) freq (i + 1))
-            Add a b ->
-                let aval = getValue progId reg (Register a)
-                    bval = getValue progId reg b
-                in processInstruction progId instr (M.insert a (aval + bval) reg) freq (i + 1)
-            Mul a b ->
-                let aval = getValue progId reg (Register a)
-                    bval = getValue progId reg b
-                in processInstruction progId instr (M.insert a (aval * bval) reg) freq (i + 1)
-            Mod a b ->
-                let aval = getValue progId reg (Register a)
-                    bval = getValue progId reg b
-                in processInstruction progId instr (M.insert a (aval `mod` bval) reg) freq (i + 1)
-            Jgz a b ->
-                if (getValue progId reg a) > 0
-                then processInstruction progId instr reg freq (i + (getValue progId reg b))
-                else processInstruction progId instr reg freq (i + 1)
 
 getValue progId reg (Number n) = n
 getValue progId reg (Register a) =
-    case M.lookup a reg of
-        Just n -> n
-        Nothing -> if a == 'p' then progId else 0
+  case M.lookup a reg of
+    Just n -> n
+    Nothing ->
+      if a == 'p'
+        then progId
+        else 0
 
 partOne = do
   instr <- parse
-  let reg = M.empty
-  return $ fmap (\i -> processInstruction 0 i reg 0 0) instr
+  let p0 = Program 0 M.empty 0 0 False False [] 0
+  return $ fmap (runProgram p0) instr
+
+runProgram p0 instr =
+  let (p0', _) = stepProgram instr p0
+  in if (finished p0' || waiting p0')
+       then lastSent p0'
+       else runProgram p0' instr
 
 partTwo = do
-    instr <- parse
-    let p0 = (0, M.empty, 0, 0, False, False, [])
-        p1 = (1, M.empty, 0, 0, False, False, [])
-
-    return $
-        fmap (runPrograms p0 p1) instr
+  instr <- parse
+  let p0 = Program 0 M.empty 0 0 False False [] 0
+      p1 = Program 1 M.empty 0 0 False False [] 0
+  return $ fmap (runPrograms p0 p1) instr
 
 runPrograms p0 p1 instr =
-    let
-        (p0', m1) = stepProgram instr p0
-        (p1', m0) = stepProgram instr p1
-    in
-        if (finished p0' && finished p1') || deadlocked p0' p1' then
-            numSent p1'
-        else
-            runPrograms (addToQueue p0' m0) (addToQueue p1' m1) instr
+  let (p0', m1) = stepProgram instr p0
+      (p1', m0) = stepProgram instr p1
+  in if (finished p0' && finished p1') || deadlocked p0' p1'
+       then sent p1'
+       else runPrograms (addToQueue p0' m0) (addToQueue p1' m1) instr
 
-addToQueue (progId, reg, i, sent, waiting, finished, q) mmsg =
-    case mmsg of
-        Nothing -> (progId, reg, i, sent, waiting, finished, q)
-        Just msg -> (progId, reg, i, sent, waiting, finished, q ++ [msg])
+addToQueue prog mmsg =
+  case mmsg of
+    Nothing -> prog
+    Just msg -> prog {queue = (queue prog) ++ [msg]}
 
+deadlocked p0 p1 = waiting p0 && waiting p1
 
-deadlocked (_, _, _, _, waiting1, _, _) (_, _, _, _, waiting2, _, _) = waiting1 && waiting2
+stepProgram instr prog@(Program { progId = progId
+                                , pos = pos
+                                , register = register
+                                , sent = sent
+                                , queue = queue
+                                })
+  | pos < 0 || pos >= (length instr) = do (prog {finished = True}, Nothing)
+  | otherwise =
+    case instr V.! pos of
+      Rcv n ->
+        case queue of
+          [] -> (prog {waiting = True}, Nothing)
+          (x:t) ->
+            ( prog
+              { register = M.insert n x register
+              , queue = t
+              , pos = pos + 1
+              , waiting = False
+              }
+            , Nothing)
+      Snd n ->
+        let send = (getValue progId register n)
+        in (prog {pos = pos + 1, sent = sent + 1, lastSent = send}, Just send)
+      Set a b ->
+        ( prog
+          { register = M.insert a (getValue progId register b) register
+          , pos = pos + 1
+          }
+        , Nothing)
+      Add a b -> binaryOp prog a b (+)
+      Mul a b -> binaryOp prog a b (*)
+      Mod a b -> binaryOp prog a b mod
+      Jgz a b ->
+        if (getValue progId register a) > 0
+          then (prog {pos = pos + (getValue progId register b)}, Nothing)
+          else (prog {pos = pos + 1}, Nothing)
 
-finished (_, _, _, _, _, f, _) = f
-
-numSent (_, _, _, n, _, _, _) = n
-
-stepProgram instr prog@(progId, reg, i, sent, waiting, finished, queue)
-    | i < 0 || i >= (length instr) = do
-        ((progId, reg, i, sent, waiting, True, queue), Nothing)
-    | otherwise =
-        case instr V.! i of
-            Rcv n ->
-                case queue of
-                    [] -> ((progId, reg, i, sent, True, False, queue), Nothing)
-                    (x:t) -> ((progId, (M.insert n x reg), (i+1), sent, False, False, t), Nothing)
-            Snd n ->
-                ((progId, reg, (i+1), (sent + 1), False, False, queue), Just (getValue progId reg n))
-            Set a b -> ((progId, (M.insert a (getValue progId reg b) reg), (i + 1), sent, False, False, queue), Nothing)
-            Add a b ->
-                binaryOp prog a b (+)
-            Mul a b ->
-                binaryOp prog a b (*)
-            Mod a b ->
-                binaryOp prog a b mod
-            Jgz a b ->
-                if (getValue progId reg a) > 0
-                then ((progId, reg, (i + (getValue progId reg b)), sent, False, False, queue), Nothing)
-                else ((progId, reg, (i + 1), sent, False, False, queue), Nothing)
-
-binaryOp (progId, reg, i, sent, waiting, finished, queue) a b op =
-    let aval = getValue progId reg (Register a)
-        bval = getValue progId reg b
-    in ((progId, (M.insert a (op aval bval) reg), (i + 1), sent, False, False, queue), Nothing)
+binaryOp prog@(Program {progId = progId, pos = pos, register = register}) a b op =
+  let aval = getValue progId register (Register a)
+      bval = getValue progId register b
+  in ( prog {register = M.insert a (op aval bval) register, pos = pos + 1}
+     , Nothing)
