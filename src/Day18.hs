5dcfe212be83c75d7d377b06391f7a20f9a08ba9
MERIDIAN\jjelfs <jjelfs@travelrepublic.co.uk>
2017-12-18 09:55:38 +0000

    day18 pt one

--- src/Day18.hs
+++ src/Day18.hs
@@ -4,6 +4,7 @@ import qualified Common as C
 import qualified Data.Map.Strict as M
 import qualified Text.Parsec as P
 import Data.Char
+import qualified Data.Vector as V
 
 data InstrType
   = Number Int
@@ -12,17 +13,17 @@ data InstrType
 
 data Instr
   = Snd InstrType
-  | Set InstrType InstrType
-  | Add InstrType InstrType
-  | Mul InstrType InstrType
-  | Mod InstrType InstrType
+  | Set Char InstrType
+  | Add Char InstrType
+  | Mul Char InstrType
+  | Mod Char InstrType
   | Rcv InstrType
   | Jgz InstrType InstrType
   deriving Show
 
 parse = do
   inp <- readFile "data/day18.txt"
-  return $ C.parse (P.sepBy instrParser (P.char '\n')) inp
+  return $ fmap V.fromList $ C.parse (P.sepBy instrParser (P.char '\n')) inp
 
 targetParser =
   P.choice [(Number <$> C.numberParser), (Register <$> P.anyChar)]
@@ -30,14 +31,45 @@ targetParser =
 instrParser =
   P.choice
     [ P.try $ P.string "snd " *> (Snd <$> targetParser)
-    , P.try $ P.string "set " *> (Set <$> (targetParser <* P.char ' ') <*> targetParser)
-    , P.try $ P.string "add " *> (Add <$> (targetParser <* P.char ' ') <*> targetParser)
-    , P.try $ P.string "mul " *> (Mul <$> (targetParser <* P.char ' ') <*> targetParser)
-    , P.try $ P.string "mod " *> (Mod <$> (targetParser <* P.char ' ') <*> targetParser)
+    , P.try $ P.string "set " *> (Set <$> (P.anyChar <* P.char ' ') <*> targetParser)
+    , P.try $ P.string "add " *> (Add <$> (P.anyChar <* P.char ' ') <*> targetParser)
+    , P.try $ P.string "mul " *> (Mul <$> (P.anyChar <* P.char ' ') <*> targetParser)
+    , P.try $ P.string "mod " *> (Mod <$> (P.anyChar <* P.char ' ') <*> targetParser)
     , P.try $ P.string "rcv " *> (Rcv <$> targetParser)
     , P.try $ P.string "jgz " *> (Jgz <$> (targetParser <* P.char ' ') <*> targetParser)
     ]
 
+processInstruction instr reg freq i
+    | i < 0 || i >= (length instr) = freq
+    | otherwise =
+        case instr V.! i of
+            Rcv n -> if (getValue reg n) > 0 then freq else (processInstruction instr reg freq (i + 1))
+            Snd n -> (processInstruction instr reg (getValue reg n) (i + 1))
+            Set a b -> (processInstruction instr (M.insert a (getValue reg b) reg) freq (i + 1))
+            Add a b ->
+                let aval = getValue reg (Register a)
+                    bval = getValue reg b
+                in processInstruction instr (M.insert a (aval + bval) reg) freq (i + 1)
+            Mul a b ->
+                let aval = getValue reg (Register a)
+                    bval = getValue reg b
+                in processInstruction instr (M.insert a (aval * bval) reg) freq (i + 1)
+            Mod a b ->
+                let aval = getValue reg (Register a)
+                    bval = getValue reg b
+                in processInstruction instr (M.insert a (aval `mod` bval) reg) freq (i + 1)
+            Jgz a b ->
+                if (getValue reg a) > 0
+                then processInstruction instr reg freq (i + (getValue reg b))
+                else processInstruction instr reg freq (i + 1)
+
+getValue reg (Number n) = n
+getValue reg (Register a) =
+    case M.lookup a reg of
+        Just n -> n
+        Nothing -> 0
+
 partOne = do
   instr <- parse
-  return "boooooring"
+  let reg = M.empty
+  return $ fmap (\i -> processInstruction i reg 0 0) instr
