f872456f4c31b714f8716909c4f142a7af67e07f
jjelfs <jjelfs@travelrepublic.co.uk>
2017-12-20 19:46:24 +0000

    used monoids to add vectors and went a bit more point free

--- src/Day20.hs
+++ src/Day20.hs
@@ -4,11 +4,10 @@ import qualified Common as C
 import Data.List
 import Data.Ord
 import qualified Text.Parsec as P
+import Data.Monoid
 
-type Position = (Int, Int, Int)
-type Velocity = (Int, Int, Int)
-type Acceleration = (Int, Int, Int)
-type Particle = (Position, Velocity, Acceleration)
+type Vector = (Int, Int, Int)
+type Particle = (Vector, Vector, Vector)
 
 parse = do
   inp <- readFile "data/day20.txt"
@@ -31,39 +30,39 @@ vecParser =
 
 partOne = do
   particles <- parse
-  return $ (fmap . fmap) snd $ fmap (\p -> scanl' tick (p, 1000000000) [0 .. 1000]) particles
+  return $
+    (fmap . fmap) snd $
+    fmap (\p -> scanl' tick (p, 1000000000) [0 .. 1000]) particles
 
 partTwo = do
   particles <- parse
-  return $ (fmap . fmap) (length . fst) $ fmap (\p -> scanl' tick (p, 1000000000) [0 .. 1000]) particles
+  return $
+    (fmap . fmap) (length . fst) $
+    fmap (\p -> scanl' tick (p, 1000000000) [0 .. 1000]) particles
 
 tick :: ([Particle], Int) -> Int -> ([Particle], Int)
 tick (particles, closest) _ =
-  let p = tickParticle <$> particles
-      dd = dedupe $ zip [0 ..] p
+  let dd = tickAndDedupe particles
   in (snd <$> dd, fst $ findClosest dd)
+  where tickAndDedupe = dedupe . (zip [0..]) . (fmap tickParticle)
 
 tickParticle :: Particle -> Particle
-tickParticle ((px, py, pz), (vx, vy, vz), acc@(ax, ay, az)) =
-  let vx' = vx + ax
-      vy' = vy + ay
-      vz' = vz + az
-      px' = px + vx'
-      py' = py + vy'
-      pz' = pz + vz'
-  in ((px', py', pz'), (vx', vy', vz'), acc)
+tickParticle (p@(px, py, pz), v@(vx, vy, vz), a@(ax, ay, az)) =
+  let v1 = tuple $ sum v <> sum a
+      p1 = tuple $ sum p <> sum v1
+  in (p1, v1, a)
+  where sum (x,y,z) = (Sum x, Sum y, Sum z)
+        tuple (Sum x, Sum y, Sum z) = (x, y, z)
 
-dedupe particles =
-    let sorted = sortParticlesByPos particles
-        groups = groupParticles sorted
-    in sortParticlesByIndex $ concat $ filter (\g -> length g == 1) groups
+dedupe =
+  (sortBy byIndex) . concat . (filter single) . (groupBy groupPos) . (sortBy comparePos)
+  where
+    comparePos (_, (p, _, _)) (_, (p1, _, _)) = compare p p1
+    groupPos (_, (p, _, _)) (_, (p1, _, _)) = p == p1
+    byIndex = comparing fst
+    single g = length g == 1
 
-sortParticlesByPos = sortBy (\(i, (p, _, _)) (i1, (p1, _, _)) -> compare p p1)
-
-sortParticlesByIndex = sortBy (comparing fst)
-
-groupParticles = groupBy (\(i, (p, _, _)) (i1, (p1, _, _)) -> p == p1)
-
-findClosest particles = minimumBy (comparing snd) $ distance <$> particles
+findClosest particles =
+  minimumBy (comparing snd) $ distance <$> particles
 
 distance (i, ((x, y, z), _, _)) = (i, (abs x) + (abs y) + (abs z))
