9797c41dbf6bdeeedf10ead377567c5f12a940e3
jjelfs <jjelfs@travelrepublic.co.uk>
2017-12-21 18:28:10 +0000

    at last

--- src/Day21.hs
+++ src/Day21.hs
@@ -1,11 +1,11 @@
 module Day21 where
 
 import qualified Common as C
+import Data.List (foldl', nub, transpose)
+import Data.List.Split (chunksOf)
+import qualified Data.Map as M
 import Data.Matrix hiding (transpose)
 import qualified Text.Parsec as P
-import Data.List.Split (chunksOf)
-import Data.List (foldl', nub, transpose)
-import qualified Data.Map as M
 
 initial = [[0, 1, 0], [0, 0, 1], [1, 1, 1]]
 
@@ -14,13 +14,14 @@ parse = do
   let m = M.empty
   return $ C.parse (fmap (collapse m) (P.sepBy (ruleParser m) (P.char '\n'))) inp
 
-ruleParser m = (,) <$> ((permutations <$> matrixParser) <* (P.string " => ")) <*> matrixParser
+ruleParser m =
+  (,) <$> (matrixParser <* (P.string " => ")) <*>
+  matrixParser
 
-collapse m rules =
-    foldl' foldPatterns m rules
+collapse m rules = foldl' foldPatterns m rules
 
-foldPatterns m (patterns, result) =
-    foldl' (\m p -> M.insert p result m) m patterns
+foldPatterns m (pattern, result) =
+  foldl' (\m p -> M.insert p result m) m (permutations pattern)
 
 matrixParser = (matrixFromList . concat) <$> P.sepBy rowParser (P.char '/')
 
@@ -41,52 +42,31 @@ rotate = reverse . transpose
 rotations = (take 4) . (iterate rotate)
 
 permutations m =
-    nub $ concat $ fmap (\r -> [r, flipHorizontally r, flipVertically r]) $ rotations m
-
-splitMatrix rules m
-    | ncols m < 4 = transform rules m
-    | mod (ncols m) 2 == 0 = splitIntoChunks rules m 2
-    | otherwise = splitIntoChunks rules m 3
-
-splitIntoChunks rules m n =
-    let s = split m
-        t = (fmap . fmap) (transform rules) s
-    in fmap join t
---    let s = ncols m
---        c = (div s n)
---        iter = [0..(c-1)]
---        processed =
---            foldl' (\row r ->
---                let col =
---                        foldl' (\col c ->
---                            let sr = (r * n) + 1
---                                sc = (c * n) + 1
---                                sub = submatrix sr (sr + (n-1)) sc (sc + (n-1)) m
---                                transformed = transform rules sub
---                            in
---                                case col of
---                                   Nothing -> Just transformed
---                                   Just col -> Just (col <|> transformed)
---                        ) Nothing iter
---                in case row of
---                    Nothing -> col
---                    Just row ->
---                        case col of
---                            Nothing -> Just row
---                            Just col -> Just (row <-> col)
---            ) Nothing iter
---    in
---        case processed of
---            Nothing -> m
---            Just p -> p
-    where split = transpose . map (map transpose . chunksOf n . transpose) . chunksOf n
-          join = transpose . concatMap (transpose . concat)
+  nub $
+  concat $ fmap (\r -> [r, flipHorizontally r, flipVertically r]) $ rotations m
 
 solve n = do
-    rules <- parse
-    return $ fmap (\r -> sum $ head $ drop n $ iterate (splitMatrix r) initial) rules
+  rules <- parse
+  return $
+    fmap (\r -> onCount $ head $ drop n $ iterate (splitMatrix r) initial) rules
+
+onCount g =
+  foldl' (\t r -> t + (sum r)) 0 g
 
 transform rules m =
-    case M.lookup m rules of
-        Nothing -> m
-        Just r -> r
+  case M.lookup m rules of
+    Nothing -> m
+    Just r -> r
+
+splitMatrix rules m
+  | length m < 4 = transform rules m
+  | mod (length m) 2 == 0 = splitIntoChunks rules m 2
+  | otherwise = splitIntoChunks rules m 3
+
+splitIntoChunks rules m n =
+  let s = split m
+      t = (fmap . fmap) (transform rules) s
+  in join t
+  where
+    split = transpose . map (map transpose . chunksOf n . transpose) . chunksOf n
+    join = transpose . concatMap (transpose . concat)
