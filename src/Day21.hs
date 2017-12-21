cdbdccd09313fa1411dc34a96ad948a3863d16d2
MERIDIAN\jjelfs <jjelfs@travelrepublic.co.uk>
2017-12-21 17:10:15 +0000

    try without matrix

--- src/Day21.hs
+++ src/Day21.hs
@@ -1,35 +1,42 @@
 module Day21 where
 
 import qualified Common as C
-import Data.Matrix
+import Data.Matrix hiding (transpose)
 import qualified Text.Parsec as P
 import Data.List.Split (chunksOf)
-import Data.List (foldl', nub)
+import Data.List (foldl', nub, transpose)
 import qualified Data.Map as M
 
-initial = fromList 3 3 [0, 1, 0, 0, 0, 1, 1, 1, 1]
+initial = [[0, 1, 0], [0, 0, 1], [1, 1, 1]]
 
 parse = do
   inp <- readFile "data/day21.txt"
-  return $ C.parse (P.sepBy ruleParser (P.char '\n')) inp
+  let m = M.empty
+  return $ C.parse (fmap (collapse m) (P.sepBy (ruleParser m) (P.char '\n'))) inp
 
-ruleParser = (,) <$> ((permutations <$> matrixParser) <* (P.string " => ")) <*> matrixParser
+ruleParser m = (,) <$> ((permutations <$> matrixParser) <* (P.string " => ")) <*> matrixParser
+
+collapse m rules =
+    foldl' foldPatterns m rules
+
+foldPatterns m (patterns, result) =
+    foldl' (\m p -> M.insert p result m) m patterns
 
 matrixParser = (matrixFromList . concat) <$> P.sepBy rowParser (P.char '/')
 
 matrixFromList list
-  | length list == 4 = fromList 2 2 list
-  | length list == 9 = fromList 3 3 list
-  | length list == 16 = fromList 4 4 list
+  | length list == 4 = toLists $ fromList 2 2 list
+  | length list == 9 = toLists $ fromList 3 3 list
+  | length list == 16 = toLists $ fromList 4 4 list
   | otherwise = error "didn't expect that"
 
 rowParser = P.many1 $ P.choice [const 1 <$> P.char '#', const 0 <$> P.char '.']
 
-flipVertically = fromLists . reverse . toLists
+flipVertically = reverse
 
-flipHorizontally = fromLists . (fmap reverse) . toLists
+flipHorizontally = fmap reverse
 
-rotate = fromLists . reverse . toLists . transpose
+rotate = reverse . transpose
 
 rotations = (take 4) . (iterate rotate)
 
@@ -42,48 +49,44 @@ splitMatrix rules m
     | otherwise = splitIntoChunks rules m 3
 
 splitIntoChunks rules m n =
-    let s = ncols m
-        c = (div s n)
-        iter = [0..(c-1)]
-        processed =
-            foldl' (\row r ->
-                let col =
-                        foldl' (\col c ->
-                            let sr = (r * n) + 1
-                                sc = (c * n) + 1
-                                sub = submatrix sr (sr + (n-1)) sc (sc + (n-1)) m
-                                transformed = transform rules sub
-                            in
-                                case col of
-                                   Nothing -> Just transformed
-                                   Just col -> Just (col <|> transformed)
-                        ) Nothing iter
-                in case row of
-                    Nothing -> col
-                    Just row ->
-                        case col of
-                            Nothing -> Just row
-                            Just col -> Just (row <-> col)
-            ) Nothing iter
-    in
-        case processed of
-            Nothing -> m
-            Just p -> p
+    let s = split m
+        t = (fmap . fmap) (transform rules) s
+    in fmap join t
+--    let s = ncols m
+--        c = (div s n)
+--        iter = [0..(c-1)]
+--        processed =
+--            foldl' (\row r ->
+--                let col =
+--                        foldl' (\col c ->
+--                            let sr = (r * n) + 1
+--                                sc = (c * n) + 1
+--                                sub = submatrix sr (sr + (n-1)) sc (sc + (n-1)) m
+--                                transformed = transform rules sub
+--                            in
+--                                case col of
+--                                   Nothing -> Just transformed
+--                                   Just col -> Just (col <|> transformed)
+--                        ) Nothing iter
+--                in case row of
+--                    Nothing -> col
+--                    Just row ->
+--                        case col of
+--                            Nothing -> Just row
+--                            Just col -> Just (row <-> col)
+--            ) Nothing iter
+--    in
+--        case processed of
+--            Nothing -> m
+--            Just p -> p
+    where split = transpose . map (map transpose . chunksOf n . transpose) . chunksOf n
+          join = transpose . concatMap (transpose . concat)
 
 solve n = do
     rules <- parse
-    let cache = M.empty
-    return $ fmap (\r -> sum $ toList $ head $ drop n $ iterate (splitMatrix r) initial) rules
+    return $ fmap (\r -> sum $ head $ drop n $ iterate (splitMatrix r) initial) rules
 
 transform rules m =
-    case foldl' (\match r@(patterns, result) ->
-                case match of
-                    Nothing -> if patternMatch r then Just result else Nothing
-                    x -> x
-            ) Nothing rules of
+    case M.lookup m rules of
         Nothing -> m
         Just r -> r
---    case filter patternMatch rules of
---        [] -> m     --no matches no transform
---        (h:_) -> snd h  --match return the result
-    where patternMatch (patterns, result) = any (\p -> p == m) patterns
