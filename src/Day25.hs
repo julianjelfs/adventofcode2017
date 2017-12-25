3df476b83316e561429aa841fb5cf65aa5b03321
jjelfs <jjelfs@travelrepublic.co.uk>
2017-12-25 12:27:09 +0000

    improved pattern match

--- src/Day25.hs
+++ src/Day25.hs
@@ -3,12 +3,7 @@ module Day25 where
 import qualified Data.Map.Strict as M
 
 data State
-  = A
-  | B
-  | C
-  | D
-  | E
-  | F
+  = A | B | C | D | E | F
   deriving Show
 
 solve =
@@ -18,31 +13,19 @@ step 12386363 (s, pos, tape) = sum (M.elems tape)
 step iterations (s, pos, tape) =
   let v = getVal pos tape
       state =
-        case s of
-          A ->
-            case v of
-              0 -> (B, pos+1, M.insert pos 1 tape)
-              1 -> (E, pos-1, M.insert pos 0 tape)
-          B ->
-            case v of
-              0 -> (C, pos-1, M.insert pos 1 tape)
-              1 -> (A, pos+1, M.insert pos 0 tape)
-          C ->
-            case v of
-              0 -> (D, pos-1, M.insert pos 1 tape)
-              1 -> (C, pos+1, M.insert pos 0 tape)
-          D ->
-            case v of
-              0 -> (E, pos-1, M.insert pos 1 tape)
-              1 -> (F, pos-1, M.insert pos 0 tape)
-          E ->
-            case v of
-              0 -> (A, pos-1, M.insert pos 1 tape)
-              1 -> (C, pos-1, M.insert pos 1 tape)
-          F ->
-            case v of
-              0 -> (E, pos-1, M.insert pos 1 tape)
-              1 -> (A, pos+1, M.insert pos 1 tape)
+        case (s, v) of
+          (A, 0) -> (B, pos+1, M.insert pos 1 tape)
+          (A, 1) -> (E, pos-1, M.insert pos 0 tape)
+          (B, 0) -> (C, pos-1, M.insert pos 1 tape)
+          (B, 1) -> (A, pos+1, M.insert pos 0 tape)
+          (C, 0) -> (D, pos-1, M.insert pos 1 tape)
+          (C, 1) -> (C, pos+1, M.insert pos 0 tape)
+          (D, 0) -> (E, pos-1, M.insert pos 1 tape)
+          (D, 1) -> (F, pos-1, M.insert pos 0 tape)
+          (E, 0) -> (A, pos-1, M.insert pos 1 tape)
+          (E, 1) -> (C, pos-1, M.insert pos 1 tape)
+          (F, 0) -> (E, pos-1, M.insert pos 1 tape)
+          (F, 1) -> (A, pos+1, M.insert pos 1 tape)
   in step (iterations + 1) state
 
 getVal pos tape =
